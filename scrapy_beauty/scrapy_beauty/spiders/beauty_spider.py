f99c9f1f180023a0c76522ae283b2f24cb13c5bc
gtechoogle <gtechoogle@gmail.com>
2017-04-15 00:20:20 +0800

    Enable to catch the key word of a home page

--- scrapy_beauty/scrapy_beauty/spiders/beauty_spider.py
+++ scrapy_beauty/scrapy_beauty/spiders/beauty_spider.py
@@ -2,7 +2,7 @@ import scrapy
 
 class BeautySpider(scrapy.Spider):
     name = 'beauty'
-    def start_requests(self, parameter_list):
+    def start_requests(self):
         urls = [
             'http://www.nanrencd.cc/'
         ]
@@ -10,4 +10,8 @@ class BeautySpider(scrapy.Spider):
             yield scrapy.Request(url=url,callback=self.parse)
     
     def parse(self, response):
-         page = response.url.split("/")[-2]
+        for post in response.xpath('//ul[@id="post_container"]//div[@class="thumbnail"]//a') :
+            yield {
+                'link': post.xpath("@href").extract_first(),
+                'title': post.xpath("@title").extract_first(),
+            }
