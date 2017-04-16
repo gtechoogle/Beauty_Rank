afa840ea6db46fbc4f6f16994ef6cb12d1f83ded
gtechoogle <gtechoogle@gmail.com>
2017-04-16 23:15:29 +0800

    demo to catch more

--- flask/app/main.py
+++ flask/app/main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index_page():
-    return render_template('index.html')
+    a = "\u5934\u6761\u5973\u795e \u5782\u9493\u7f8e\u4eba \u6a0a\u5b89\u59ae"
+    print a.encode("UTF-8");
+    return a;#render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scrapy_beauty/scrapy_beauty/spiders/beauty_spider.py
+++ scrapy_beauty/scrapy_beauty/spiders/beauty_spider.py
@@ -8,10 +8,39 @@ class BeautySpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
-    
     def parse(self, response):
         for post in response.xpath('//ul[@id="post_container"]//div[@class="thumbnail"]//a') :
+            # yield {
+            #     'title': post.xpath("@title").extract_first(),
+            #     'link': post.xpath("@href").extract_first(),
+            # }
+            details_link = response.xpath('//ul[@id="post_container"]//div[@class="thumbnail"]//a/@href').extract_first();
+            print post.xpath("@title").extract_first()
+            print details_link
+            if details_link is not None:
+                print "#######"
+                yield scrapy.Request(details_link, callback=self.parse_image)
+    
+    # def parse(self, response):
+    #     for post in response.xpath('//ul[@id="post_container"]//div[@class="thumbnail"]//a') :
+    #         yield {
+    #             #'link': post.xpath("@href").extract_first(),
+    #             #'title': post.xpath("@title").extract_first()
+    #         }
+    #     details_link = response.xpath('//ul[@id="post_container"]//div[@class="thumbnail"]//a/@href').extract_first();
+    #     if details_link is not None:
+    #         yield scrapy.Request(details_link, callback=self.parse_image)
+    #     next_page = response.xpath('//div[@class="pagination"]/a[@class="next"]/@href').extract_first()
+    #     if next_page is not None:
+    #         yield scrapy.Request(next_page, callback=self.parse)
+    
+    def parse_image(self, response):
+        print "parse_image"
+        for img_link in response.xpath('//div[@id="post_content"]//img'):
             yield {
-                'link': post.xpath("@href").extract_first(),
-                'title': post.xpath("@title").extract_first(),
+                'image_link': img_link.xpath("@src").extract_first()
             }
+        next_image = response.xpath('//li[@class="page-next"]//a/@href').extract_first()
+        if next_image is not None:
+            yield scrapy.Request(next_image, callback=self.parse_image);
+
